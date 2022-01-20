e341c9bc0c9d44fb2b0f1c69a324ac71a531b80c
Timur Kabaev <timur_kabaev@epam.com>
2022-01-20 10:36:03 +0300

    feature_02: refactor to multimodule project

---
+++ keeper-service-infrastructure/src/main/java/com/myorg/KeeperServiceInfrastructureStack.java
@@ -0,0 +1,59 @@
+package com.myorg;
+
+import software.amazon.awscdk.CfnParameter;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.patterns.*;
+
+public class KeeperServiceInfrastructureStack extends Stack {
+
+    public KeeperServiceInfrastructureStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public KeeperServiceInfrastructureStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+//        CfnParameter ecrKeeperRepoName = CfnParameter.Builder.create(this, "ecrKeeperRepoName")
+//                .build();
+//
+//        CfnParameter imageTagName = CfnParameter.Builder.create(this, "imageTag")
+//                .build();
+
+        Vpc vpc = Vpc.Builder.create(this, "MyVpc")
+                .maxAzs(3)
+                .build();
+
+        Cluster cluster = Cluster.Builder.create(this, "MyCluster")
+                .vpc(vpc)
+                .build();
+
+        ApplicationLoadBalancedFargateService.Builder.create(this, "MyFargateService")
+                .cluster(cluster)
+                .assignPublicIp(true)
+                .desiredCount(1)
+                .cpu(512)
+                .memoryLimitMiB(1024)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .image(
+                                        ContainerImage.fromEcrRepository(
+                                                Repository.fromRepositoryName(
+                                                        this,
+                                                        "EcrKeeperRepo",
+                                                        "keeper-repo"
+//                                                        ecrKeeperRepoName.getValueAsString()
+                                                ),
+                                                "latest"
+//                                                imageTagName.getValueAsString()
+                                        )
+                                )
+                                .containerPort(8080)
+                                .build())
+                .build();
+    }
+}
