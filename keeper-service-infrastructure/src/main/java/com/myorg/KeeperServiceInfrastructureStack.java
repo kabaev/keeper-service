95934933b4005f987e61333fccf8c83604e103f4
Timur Kabaev <timur_kabaev@epam.com>
2022-01-24 20:41:52 +0300

    feature_06: add database support

--- keeper-service-infrastructure/src/main/java/com/myorg/KeeperServiceInfrastructureStack.java
+++ keeper-service-infrastructure/src/main/java/com/myorg/KeeperServiceInfrastructureStack.java
@@ -1,10 +1,9 @@
 package com.myorg;
 
-import software.amazon.awscdk.CfnParameter;
+import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.rds.*;
 import software.constructs.Construct;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.*;
@@ -24,10 +23,41 @@ public class KeeperServiceInfrastructureStack extends Stack {
         CfnParameter imageTagName = CfnParameter.Builder.create(this, "imageTagName")
                 .build();
 
+        CfnParameter postgresDatabaseName = CfnParameter.Builder.create(this, "postgresDatabaseName")
+                .defaultValue("keeperdb")
+                .build();
+
+        CfnParameter postgresUserName = CfnParameter.Builder.create(this, "postgresUserName")
+                .defaultValue("userdb")
+                .build();
+
+        var postgresUserSecret = DatabaseSecret.Builder.create(this, "PostgresCredentials")
+                .username(postgresUserName.getValueAsString())
+                .build();
+
         Vpc vpc = Vpc.Builder.create(this, "VPC")
                 .maxAzs(3)
                 .subnetConfiguration(Vpc.DEFAULT_SUBNETS_NO_NAT)
                 .build();
+
+        var postgres = DatabaseInstance.Builder.create(this, "Postgres")
+                .vpc(vpc)
+                .vpcSubnets(SubnetSelection.builder().subnetType(SubnetType.PRIVATE_ISOLATED).build())
+                .engine(
+                        DatabaseInstanceEngine.postgres(
+                                PostgresInstanceEngineProps.builder()
+                                        .version(PostgresEngineVersion.VER_13_4)
+                                        .build()
+                        )
+                )
+                .credentials(Credentials.fromSecret(postgresUserSecret))
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO))
+                .databaseName(postgresDatabaseName.getValueAsString())
+                .backupRetention(Duration.days(0))
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+
+        postgres.getConnections().allowFromAnyIpv4(Port.tcp(5432), "Allow connections to the database");
 
         Cluster cluster = Cluster.Builder.create(this, "MyCluster")
                 .vpc(vpc)
