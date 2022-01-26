f81a87dc23c0a2690763feb8b7b617f6f8f44fe7
Timur Kabaev <timur_kabaev@epam.com>
2022-01-26 15:04:23 +0300

    feature_07: connect to remote and local databases

---
+++ keeper-service-app/src/main/java/com/kabaev/shop/service/keeper/domain/Product.java
@@ -0,0 +1,33 @@
+package com.kabaev.shop.service.keeper.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Product {
+
+    @Id
+    @SequenceGenerator(
+            name = "product_sequence",
+            sequenceName = "product_sequence"
+    )
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long id;
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+}
--- keeper-service-infrastructure/src/main/java/com/myorg/KeeperServiceInfrastructureStack.java
+++ keeper-service-infrastructure/src/main/java/com/myorg/KeeperServiceInfrastructureStack.java
@@ -2,11 +2,15 @@ package com.myorg;
 
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.amazon.awscdk.services.rds.*;
 import software.constructs.Construct;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.*;
+
+import java.util.Map;
 
 public class KeeperServiceInfrastructureStack extends Stack {
 
@@ -40,19 +44,19 @@ public class KeeperServiceInfrastructureStack extends Stack {
 
         var keeperDatabaseName = CfnParameter.Builder.create(this, "keeperDatabaseName")
                 .description("The name of the Keeper Service Database keeping information about all products.")
-                .defaultValue("keeper_db")
+                .defaultValue("keeperdb")
                 .build();
 
         var keeperDatabaseUserName = CfnParameter.Builder.create(this, "keeperDatabaseUserName")
                 .description("The name of the Keeper Service Database user.")
-                .defaultValue("user_db")
+                .defaultValue("managerdb")
                 .build();
 
         var keeperDatabaseUserSecret = DatabaseSecret.Builder.create(this, "keeperDatabaseUserSecret")
                 .username(keeperDatabaseUserName.getValueAsString())
                 .build();
 
-        var postgres = DatabaseInstance.Builder.create(this, "Postgres")
+        var postgres = DatabaseInstance.Builder.create(this, "postgres")
                 .engine(
                         DatabaseInstanceEngine.postgres(
                                 PostgresInstanceEngineProps.builder()
@@ -77,6 +81,10 @@ public class KeeperServiceInfrastructureStack extends Stack {
                 .vpc(vpc)
                 .build();
 
+        var taskRole = Role.Builder.create(this, "taskRole")
+                .assumedBy(ServicePrincipal.Builder.create("ecs-tasks.amazonaws.com").build())
+                .build();
+
         ApplicationLoadBalancedFargateService.Builder.create(this, "keeperServiceFargate")
                 .cluster(cluster)
                 .assignPublicIp(true)
@@ -85,6 +93,17 @@ public class KeeperServiceInfrastructureStack extends Stack {
                 .memoryLimitMiB(MEMORY_LIMIT_AMOUNT)
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("service")
+                                .taskRole(taskRole)
+                                .environment(Map.of(
+                                        "POSTGRES_HOST", postgres.getDbInstanceEndpointAddress(),
+                                        "POSTGRES_PORT", postgres.getDbInstanceEndpointPort(),
+                                        "POSTGRES_DATABASE", keeperDatabaseName.getValueAsString()
+                                ))
+                                .secrets(Map.of(
+                                        "POSTGRES_USER", Secret.fromSecretsManager(keeperDatabaseUserSecret, "username"),
+                                        "POSTGRES_PASSWORD", Secret.fromSecretsManager(keeperDatabaseUserSecret, "password")
+                                ))
                                 .image(
                                         ContainerImage.fromEcrRepository(
                                                 Repository.fromRepositoryName(
