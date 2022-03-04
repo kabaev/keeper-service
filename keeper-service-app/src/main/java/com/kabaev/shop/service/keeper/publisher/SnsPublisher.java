18f6744bb7451a7fff5863d21851a5d0d60f5fa1
Timur Kabaev <timur_kabaev@epam.com>
2022-03-04 11:30:35 +0300

    feature_22: add sqs to localstack cloud

--- keeper-service-app/src/main/java/com/kabaev/shop/service/keeper/publisher/SnsPublisher.java
+++ keeper-service-app/src/main/java/com/kabaev/shop/service/keeper/publisher/SnsPublisher.java
@@ -2,39 +2,28 @@ package com.kabaev.shop.service.keeper.publisher;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
-
-import java.net.URI;
 
 @Component
 public class SnsPublisher {
 
-    private final String regionName;
-    private final String topicArn;
-    private final String awsEndpoint;
+    private final SnsClient snsClient;
+    private final String snsTopicArn;
 
     public SnsPublisher(
-            @Value("${s3.region.name}") String regionName,
-            @Value("${sns.topic.arn}") String topicArn,
-            @Value("${sns.endpoint}") String awsEndpoint) {
-        this.regionName = regionName;
-        this.topicArn = topicArn;
-        this.awsEndpoint = awsEndpoint;
+            SnsClient snsClient,
+            @Value("${sns.topic.arn}") String snsTopicArn) {
+        this.snsClient = snsClient;
+        this.snsTopicArn = snsTopicArn;
     }
 
     public void sendInTopic(String code) {
-        Region region = Region.of(regionName);
-        SnsClient snsClient = SnsClient.builder()
-                .endpointOverride(URI.create(awsEndpoint))
-                .region(region)
-                .build();
         PublishRequest request = PublishRequest.builder()
-                .message(code)
-                .topicArn(topicArn)
-                .messageGroupId(code)
+                .messageGroupId("GroupId")
                 .messageDeduplicationId(code)
+                .message(code)
+                .topicArn(snsTopicArn)
                 .build();
         snsClient.publish(request);
     }
